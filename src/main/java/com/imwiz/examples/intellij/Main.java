a0274e012efd59fe4c66c00291f2f4b69695ba78
falenn <dev@imwiz.com>
2018-09-18 14:32:12 +0000

    testing github issue / branch naming and linking

--- src/main/java/com/imwiz/examples/intellij/Main.java
+++ src/main/java/com/imwiz/examples/intellij/Main.java
@@ -4,5 +4,9 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello World");
+
+        //comment to commit
+        System.out.println("Good stuff");
+
     }
 }
