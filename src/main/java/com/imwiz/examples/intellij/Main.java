cd0b1ba211681bc3bc91df55ef71d561a87124a5
falenn <dev@imwiz.com>
2018-09-18 14:39:28 +0000

    here

--- src/main/java/com/imwiz/examples/intellij/Main.java
+++ src/main/java/com/imwiz/examples/intellij/Main.java
@@ -2,11 +2,14 @@ package com.imwiz.examples.intellij;
 
 public class Main {
 
-    public static void main(String[] args) {
-        System.out.println("Hello World");
+  /**
+   * Main Method.
+   * @param args as commandlline parameters of type [@link java.lang.String].
+   */
+  public static void main(String[] args) {
+    System.out.println("Hello World");
 
-        //comment to commit
-        System.out.println("Good stuff");
-
-    }
+    //comment to commit
+    System.out.println("Good stuff");
+  }
 }
